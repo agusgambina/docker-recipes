da11a448827fd6dc27874466acb5460109ca2ae7
Agustin Gambina <agusgambina@gmail.com>
2019-12-31 15:27:20 -0300

    p2p dockerized

---
+++ p2p/removeP2p.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+rm -rf /home/$USER/storage/downloads/p2p/torrents/config
+rm -rf /home/$USER/storage/downloads/p2p/torrents/incoming
+rm -rf /home/$USER/storage/downloads/p2p/torrents/watch
+rm -rf /home/$USER/storage/downloads/p2p/amule/conf
+rm -rf /home/$USER/storage/downloads/p2p/amule/temp
+rm -rf /home/$USER/storage/downloads/p2p/amule/incoming
---
+++ p2p/setupP2p.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+mkdir -p /home/$USER/storage/downloads/p2p/torrents/config
+mkdir -p /home/$USER/storage/downloads/p2p/torrents/incoming
+mkdir -p /home/$USER/storage/downloads/p2p/torrents/watch
+mkdir -p /home/$USER/storage/downloads/p2p/amule/conf
+mkdir -p /home/$USER/storage/downloads/p2p/amule/temp
+mkdir -p /home/$USER/storage/downloads/p2p/amule/incoming
