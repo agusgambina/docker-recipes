64726d5552ea71ad821e282b77a0d77593f7ebd4
Agustin Gambina <agusgambina@gmail.com>
2019-12-31 14:52:30 -0300

    plex dockerized

---
+++ plex/removePlex.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+rm -rf /home/$USER/storage/config/PlexMediaServer/config
+rm -rf /home/$USER/storage/media/video
+rm -rf /home/$USER/storage/media
---
+++ plex/setupPlex.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+mkdir -p /home/$USER/storage/config/PlexMediaServer/config
+mkdir -p /home/$USER/storage/media/video
+mkdir -p /home/$USER/storage/media
