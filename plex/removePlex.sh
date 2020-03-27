3d932f9be15e1601de2c5dc432d60caf4635b93c
Agustin Gambina <agusgambina@gmail.com>
2020-03-27 10:59:47 +0000

    add flexget

--- plex/removePlex.sh
+++ plex/removePlex.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 rm -rf /home/$USER/storage/config/PlexMediaServer/config
-rm -rf /home/$USER/storage/media/video
+rm -rf /home/$USER/storage/media/transcode
 rm -rf /home/$USER/storage/media
